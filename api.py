ef60e1d61d46c3f2c28dd4b1f618b354c994dd0d
ARKDevelopment <111500885+ARKDevelopment@users.noreply.github.com>
2022-09-05 13:27:19 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,154 @@
+import csv
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from starlette.responses import FileResponse
+from pydantic import BaseModel
+import sqlite3
+import uuid
+
+
+app = FastAPI()
+
+
+conn = sqlite3.connect('autoinsurance.db')
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS queue (
+id TEXT,
+first_name TEXT,
+last_name TEXT,
+street_address TEXT,
+city TEXT,
+zip TEXT,
+phone TEXT,
+email TEXT
+); """
+)
+
+conn.commit()
+conn.close()
+
+
+class AutoInsurance(BaseModel):
+    first_name: str
+    last_name: str
+    street_address: str
+    city: str 
+    zipp: str
+    phone: str
+    email: str
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Logs</title>
+    </head>
+    <body>
+        <div>
+        <a href='/download' style='float:right'>Download csv</a>
+        </div>
+        <h3>Id, First Name, Last Name, Street Address, Zip, Phone, Email, Status</h2>
+        <div id="log-body"></div>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('log-body')
+                var content = document.createTextNode(event.data)
+                messages.innerHTML = content.textContent
+            };
+             setInterval(() => ws.send('sefesfae'), 100)
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+      data = await websocket.receive_text()
+      con = sqlite3.connect('autoinsurance.db')
+      cur = con.cursor()
+      cmd = cur.execute("SELECT * FROM log")
+      data = cmd.fetchall()
+      new_data = "<br/>".join([", ".join(x) for x in data])
+      con.close()
+      await websocket.send_text(new_data)
+
+
+@app.get('/queued')
+def queued():
+    con = sqlite3.connect('autoinsurance.db')
+    cur = con.cursor()
+    cmd = cur.execute("SELECT * FROM queue")
+    data = cmd.fetchall()
+    con.close()
+    return data
+
+
+@app.post('/add-to-queue')
+async def automate(auto_insurance: AutoInsurance):
+  idd = str(uuid.uuid4())
+  con = sqlite3.connect('autoinsurance.db')
+  cur = con.cursor()
+  cur.execute(f"""INSERT INTO queue (
+    id, 
+    first_name, 
+    last_name, 
+    street_address, 
+    city, 
+    zip, 
+    phone, 
+    email
+    ) 
+    VALUES ({"?, "*7}?)""", (
+        idd, 
+        auto_insurance.first_name, 
+        auto_insurance.last_name,  
+        auto_insurance.street_address, 
+        auto_insurance.city, 
+        auto_insurance.zipp, 
+        auto_insurance.phone, 
+        auto_insurance.email))
+  con.commit()
+  con.close()
+  return auto_insurance
+
+@app.get('/download')
+def download_as_csv():
+    con = sqlite3.connect('autoinsurance.db')
+    cur = con.cursor()
+    cmd = cur.execute("SELECT * FROM log")
+    
+    with open('logs.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(
+            [
+                "Id", 
+                "First Name", 
+                "Last Name", 
+                "Street Address", 
+                "zip", 
+                "Phone", 
+                "email", 
+                "Year", 
+                "Make", 
+                "Model", 
+                "Insured From", 
+                "DOB", 
+                "Gender", 
+                "Device", 
+                "Education", 
+                "Rating", 
+                "Status"
+            ]
+        )
+        writer.writerows(cmd)
+    return FileResponse('logs.csv', media_type='text/csv', filename='logs.csv')
---
+++ main.py
@@ -0,0 +1,188 @@
+import asyncio
+import json
+import random, datetime, requests
+from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError 
+
+first_name  = "Hector"
+last_name   = "Hector"
+street_address = "1234 Main St"
+zipp = "10005"
+phone = "1234567890"
+email = "heck@tor.com"
+
+PERCENT = 50
+percent = PERCENT
+
+
+def genderize(name):
+  req = requests.get(f"https://api.genderize.io/?name={name}").text
+  return eval(req)["gender"].title()
+
+def ziptostate(zip):
+  req = requests.get(f"https://api.zippopotam.us/us/{zip}").text
+  return eval(req)["places"][0]["state"]
+
+
+def playwright_devices():
+  req = requests.get("https://raw.githubusercontent.com/microsoft/playwright/main/packages/playwright-core/src/server/deviceDescriptorsSource.json").text
+  return json.loads(req)
+
+async def emulated_browser(playwright, proxy=None):
+  playwright_device_list = playwright_devices()
+  device_list = [x for x in playwright_device_list if x.split(" ")[-1] != "landscape"]
+  global random_device
+  random_device = random.choice(device_list)
+  print(random_device)
+  device = playwright.devices[random_device]
+  browser = await playwright.chromium.launch(headless=False)
+  # browser = await playwright[playwright_device_list[random_device]["defaultBrowserType"]].launch(headless=False)
+  return await browser.new_context(**device, 
+    proxy={**proxy} if proxy else None
+  )
+  
+
+async def random_selector(page, selector):
+  item = await page.query_selector_all(f'{selector} > option')
+  item = item[1:]
+  item = random.choice(item)
+  item = await item.get_attribute('value')
+  await page.select_option(selector, item)
+  return item
+
+async def scroller(page, wait):
+  while wait > 0:
+      print(wait)
+      about_us = await page.query_selector('[data-id="475a35b7"]')
+      await about_us.scroll_into_view_if_needed()
+      rem = random.randint(2000, 5000)
+      await page.wait_for_timeout(rem)
+      wait -= rem
+      foot = await page.query_selector('footer')
+      await foot.scroll_into_view_if_needed()
+      rem = random.randint(2000, 5000)
+      await page.wait_for_timeout(rem)
+      wait -= rem
+
+
+async def main(first_name, last_name, street_address, city, zipp, phone, email):
+  async with async_playwright() as p:
+    browser = await emulated_browser(p)
+    # return [x for x in range(9)]
+    page = await browser.new_page()
+    try:
+      await page.goto("https://auto.saveonautoinsurance.us/", timeout=60000)
+    except PlaywrightTimeoutError:
+      await page.wait_for_selector('#year', timeout=60000)
+
+    wait = random.randint(20000, 120000)
+    
+    year = random.randint(2012, int(datetime.datetime.today().year))
+    await page.select_option('#year', str(year))
+    await page.evaluate('loadVehiclMakes()')
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    
+    make = await random_selector(page, '#make')
+    await page.evaluate('loadModels()')
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    model = await random_selector(page, '#model')
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    insuredform = await random_selector(page, '#insuredform')
+
+    await page.check('#leadid_tcpa_disclosure')
+
+    tm = random.choice([False * 100-percent, True * percent])
+    if tm:
+      page2 = await browser.new_page()
+      await page2.goto('https://auto.saveonautoinsurance.us/terms.html', timeout=60000)
+      ps = await page2.query_selector_all('p')
+      for p in ps:
+        if wait < 1:
+          break
+        await p.scroll_into_view_if_needed()
+        rem = random.randint(2000, 3000)
+        await page.wait_for_timeout(rem)
+        wait -= rem
+      await page2.close()
+
+    await scroller(page, wait)
+    await page.click('#submit')
+
+    #PAGE 2
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#firstname', first_name, delay=random.randint(20, 120))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#lastname', last_name, delay=random.randint(20, 120))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    dob = "/".join(map(str, [random.randint(1,12),random.randint(1, 28),int(datetime.datetime.today().year) - random.randint(23, 61)]))
+    await page.type('#dateofbirth', dob, delay=random.randint(90, 200))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+    await page.click('.ui-state-active')
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    gender = genderize(first_name)
+    await page.select_option('#gender', gender)
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#streetaddress', street_address, delay=random.randint(70, 200))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#zip', zipp, delay=random.randint(20, 120))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#phone', phone, delay=random.randint(20, 120))
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.type('#email', email, delay=random.randint(20, 120))
+
+    await page.check('#liketoreceive')
+
+
+    h3 = await page.query_selector('h3')
+    await h3.scroll_into_view_if_needed()
+    await page.wait_for_timeout(random.randint(2000, 5000))
+
+    h3 = await page.query_selector('h3 >> nth=1')
+    await h3.scroll_into_view_if_needed()
+    await page.wait_for_timeout(random.randint(2000, 5000))
+
+    
+
+    #Part 2
+    education = await random_selector(page, '#education')
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    rating = random.choice(['Good', 'Excellent'])
+    await page.select_option('#creditrating', rating)
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.select_option('#married', "No")
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+    await page.select_option('#tickets', "No")
+    await page.wait_for_timeout(random.randint(1000, 2000))
+
+
+    submit_button = await page.query_selector('#submit')
+    await submit_button.scroll_into_view_if_needed()
+    await page.wait_for_timeout(random.randint(2000, 5000))
+    await submit_button.click()
+
+    #PAGE 3
+    await page.wait_for_timeout(random.randint(10000, 30000))
+
+    return year, make, model, insuredform, dob, gender, education, rating, random_device
+
+
+
+if __name__ == "__main__":
+  asyncio.run(main())
+  # print(playwright_devices())
+  # print(genderize('John'))
