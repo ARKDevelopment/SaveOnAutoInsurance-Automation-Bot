c909f0240b4a1c831dddee89ed86f58fa8401ef6
ARKDevelopment <111500885+ARKDevelopment@users.noreply.github.com>
2022-09-20 22:25:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -78,8 +78,9 @@ async def main(first_name, last_name, street_address, city, zipp, phone, email):
     # return [x for x in range(9)]
 
     page = await browser.new_page()
-    await page.goto("http://auto.saveyourinsurance.com/")
+    await page.goto("http://auto.saveyourinsurance.com/", timeout=60000)
     # await page.goto("http://ifconfig.me/")
+    # input("KKK")
 
     await page.wait_for_selector('#year')
 
