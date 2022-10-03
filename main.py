cafeafcdeb78e43688fbe1bdd9966a7697ceea9c
ARKDevelopment <111500885+ARKDevelopment@users.noreply.github.com>
2022-10-03 21:44:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def random_selector(page, selector):
 async def scroller(page, wait):
   while wait > 0:
       print('AU')
-      about_us = await page.query_selector('#liketoreceive')
+      about_us = await page.query_selector('h1')
       await about_us.scroll_into_view_if_needed()
       rem = random.randint(2000, 5000)
       await page.wait_for_timeout(rem)
